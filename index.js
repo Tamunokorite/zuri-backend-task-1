39d40424cf847abc00c9126a5e09d11664d71748
Tamunokorite <obriggs03@gmail.com>
2023-09-11 10:49:56 +0100

    Changes

--- index.js
+++ index.js
@@ -12,17 +12,21 @@ app.use(cors({origin: true}))
 
 const today = new Date();
 const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-app.get("/", (req, res) => {
+app.get("/api", (req, res) => {
     const { slack_name, track } = req.query;
-    res.status(200).json({
-        slack_name,
-        current_day: days[today.getDay()],
-        utc_time: today.toISOString(),
-        track,
-        github_file_url: "https://github.com/Tamunokorite/zuri-backend-task-1/blob/main/index.js",
-        github_repo_url: "https://github.com/Tamunokorite/zuri-backend-task-1",
-        status_code: 200
-    });
+    if (slack_name && track) {
+        res.status(200).json({
+            slack_name: slack_name,
+            current_day: days[today.getDay()],
+            utc_time: today.toISOString(),
+            track: track,
+            github_file_url: "https://github.com/Tamunokorite/zuri-backend-task-1/blob/main/index.js",
+            github_repo_url: "https://github.com/Tamunokorite/zuri-backend-task-1",
+            status_code: 200
+        });
+    } else {
+        res.status(400).send("Invalid request");
+    }
 });
 
 const PORT = process.env.PORT || 5000;
