4cca23d6692f67fc574f6a1df541f1c5efa3cec1
Tamunokorite <obriggs03@gmail.com>
2023-09-11 10:59:42 +0100

    Fixed UTC time issue

--- index.js
+++ index.js
@@ -8,7 +8,19 @@ const app = express();
 
 app.use(express.json());
 app.use(bodyParser.json());
-app.use(cors({origin: true}))
+app.use(cors({origin: true}));
+
+function getCurrentUTCTime() {
+    const now = new Date();
+    const year = now.getUTCFullYear();
+    const month = String(now.getUTCMonth() + 1).padStart(2, '0'); // Months are zero-indexed
+    const day = String(now.getUTCDate()).padStart(2, '0');
+    const hours = String(now.getUTCHours()).padStart(2, '0');
+    const minutes = String(now.getUTCMinutes()).padStart(2, '0');
+    const seconds = String(now.getUTCSeconds()).padStart(2, '0');
+    
+    return `${year}-${month}-${day}T${hours}:${minutes}:${seconds}Z`;
+  }
 
 const today = new Date();
 const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
@@ -18,7 +30,7 @@ app.get("/api", (req, res) => {
         res.status(200).json({
             slack_name: slack_name,
             current_day: days[today.getDay()],
-            utc_time: today.toISOString(),
+            utc_time: getCurrentUTCTime(),
             track: track,
             github_file_url: "https://github.com/Tamunokorite/zuri-backend-task-1/blob/main/index.js",
             github_repo_url: "https://github.com/Tamunokorite/zuri-backend-task-1",
